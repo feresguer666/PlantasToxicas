67c87e4d8edda5761e7dd749c019b29bc58281dc
TuNombre <tuemail@gmail.com>
2026-05-28 01:40:46 +0200

    feat: nueva funcionalidad de búsqueda por síntomas...

--- app/src/main/java/com/toxicplants/database/ui/navigation/PlantNavGraph.kt
+++ app/src/main/java/com/toxicplants/database/ui/navigation/PlantNavGraph.kt
@@ -8,38 +8,22 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.toxicplants.database.PlantEntity
 import com.toxicplants.database.ui.viewmodel.PlantViewModel
-import com.toxicplants.database.ui.screens.HomeScreen
-import com.toxicplants.database.ui.screens.PlantListScreen
-import com.toxicplants.database.ui.screens.PlantDetailScreen
-import com.toxicplants.database.ui.screens.CategoriesScreen
-import com.toxicplants.database.ui.screens.EmergencyScreen
-import com.toxicplants.database.ui.screens.SearchScreen
-import com.toxicplants.database.ui.screens.DownloadImagesScreen
-import com.toxicplants.database.ui.screens.EditPlantScreen
-import com.toxicplants.database.ui.screens.OnlineDatabasesScreen
-import com.toxicplants.database.ui.screens.CameraIdentifyScreen
-import com.toxicplants.database.ui.screens.PlantNetResultScreen
+import com.toxicplants.database.ui.screens.*
 
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object PlantList : Screen("plant_list")
-    object PlantDetail : Screen("plant_detail/{plantId}") {
-        fun createRoute(plantId: Int) = "plant_detail/$plantId"
-    }
+    object PlantDetail : Screen("plant_detail/{plantId}") { fun createRoute(plantId: Int) = "plant_detail/$plantId" }
     object Categories : Screen("categories")
     object Emergency : Screen("emergency")
     object OnlineDatabases : Screen("online_databases")
     object Search : Screen("search")
+    object SearchBySymptoms : Screen("search_symptoms")
     object DownloadImages : Screen("download_images")
     object NewPlant : Screen("new_plant")
-    object EditPlant : Screen("edit_plant/{plantId}") {
-        fun createRoute(plantId: Int) = "edit_plant/$plantId"
-    }
+    object EditPlant : Screen("edit_plant/{plantId}") { fun createRoute(plantId: Int) = "edit_plant/$plantId" }
     object CameraIdentify : Screen("camera_identify")
-    object PlantNetResult : Screen("plantnet_result/{name}/{scientificName}") {
-        fun createRoute(name: String, scientificName: String) = "plantnet_result/$name/$scientificName"
-    }
-    object SearchBySymptoms : Screen("search_symptoms")
+    object PlantNetResult : Screen("plantnet_result/{name}/{scientificName}") { fun createRoute(name: String, scientificName: String) = "plantnet_result/$name/$scientificName" }
 }
 
 @Composable
@@ -55,11 +39,12 @@ fun PlantNavGraph(viewModel: PlantViewModel) {
                 onNavigateToEmergency = { navController.navigate(Screen.Emergency.route) },
                 onNavigateToOnlineDatabases = { navController.navigate(Screen.OnlineDatabases.route) },
                 onNavigateToSearch = { navController.navigate(Screen.Search.route) },
+                onNavigateToSearchBySymptoms = { navController.navigate(Screen.SearchBySymptoms.route) },
                 onNavigateToDownloadImages = { navController.navigate(Screen.DownloadImages.route) },
                 onNavigateToNewPlant = { navController.navigate(Screen.NewPlant.route) },
                 onNavigateToCamera = { navController.navigate(Screen.CameraIdentify.route) },
-                onNavigateToPhytochemistry = { /* TODO */ },
-                onNavigateToSettings = { /* TODO */ },
+                onNavigateToPhytochemistry = { },
+                onNavigateToSettings = { },
                 onPlantClick = { plant: PlantEntity ->
                     viewModel.selectPlant(plant)
                     navController.navigate(Screen.PlantDetail.createRoute(plant.id))
@@ -68,78 +53,29 @@ fun PlantNavGraph(viewModel: PlantViewModel) {
         }
 
         composable(Screen.PlantList.route) {
-            PlantListScreen(
-                viewModel = viewModel,
-                onPlantClick = { plant: PlantEntity ->
-                    viewModel.selectPlant(plant)
-                    navController.navigate(Screen.PlantDetail.createRoute(plant.id))
-                },
-                onBack = { navController.popBackStack() }
-            )
+            PlantListScreen(viewModel = viewModel, onPlantClick = { plant: PlantEntity -> viewModel.selectPlant(plant); navController.navigate(Screen.PlantDetail.createRoute(plant.id)) }, onBack = { navController.popBackStack() })
         }
 
-        composable(
-            route = Screen.PlantDetail.route,
-            arguments = listOf(navArgument("plantId") { type = NavType.IntType })
-        ) { backStackEntry ->
+        composable(route = Screen.PlantDetail.route, arguments = listOf(navArgument("plantId") { type = NavType.IntType })) { backStackEntry ->
             val plantId = backStackEntry.arguments?.getInt("plantId") ?: return@composable
-            PlantDetailScreen(
-                plantId = plantId,
-                viewModel = viewModel,
-                onBack = { navController.popBackStack() },
-                onEdit = { id -> navController.navigate(Screen.EditPlant.createRoute(id)) }
-            )
+            PlantDetailScreen(plantId = plantId, viewModel = viewModel, onBack = { navController.popBackStack() }, onEdit = { id -> navController.navigate(Screen.EditPlant.createRoute(id)) })
         }
 
         composable(Screen.Categories.route) {
-            CategoriesScreen(
-                viewModel = viewModel,
-                onCategoryClick = { category: String ->
-                    viewModel.setCategory(category)
-                    navController.navigate(Screen.PlantList.route)
-                },
-                onBack = { navController.popBackStack() }
-            )
+            CategoriesScreen(viewModel = viewModel, onCategoryClick = { category: String -> viewModel.setCategory(category); navController.navigate(Screen.PlantList.route) }, onBack = { navController.popBackStack() })
         }
 
         composable(Screen.Emergency.route) {
-            EmergencyScreen(
-                viewModel = viewModel,
-                onPlantClick = { plant: PlantEntity ->
-                    viewModel.selectPlant(plant)
-                    navController.navigate(Screen.PlantDetail.createRoute(plant.id))
-                },
-                onBack = { navController.popBackStack() }
-            )
+            EmergencyScreen(viewModel = viewModel, onPlantClick = { plant: PlantEntity -> viewModel.selectPlant(plant); navController.navigate(Screen.PlantDetail.createRoute(plant.id)) }, onBack = { navController.popBackStack() })
         }
 
-        composable(Screen.OnlineDatabases.route) {
-            OnlineDatabasesScreen(onNavigateBack = { navController.popBackStack() })
-        }
+        composable(Screen.OnlineDatabases.route) { OnlineDatabasesScreen(onNavigateBack = { navController.popBackStack() }) }
+        composable(Screen.Search.route) { SearchScreen(viewModel = viewModel, onPlantClick = { plant: PlantEntity -> viewModel.selectPlant(plant); navController.navigate(Screen.PlantDetail.createRoute(plant.id)) }, onBack = { navController.popBackStack() }) }
+        composable(Screen.SearchBySymptoms.route) { SearchBySymptomsScreen(viewModel = viewModel, onPlantClick = { plant: PlantEntity -> viewModel.selectPlant(plant); navController.navigate(Screen.PlantDetail.createRoute(plant.id)) }, onBack = { navController.popBackStack() }) }
+        composable(Screen.DownloadImages.route) { DownloadImagesScreen(viewModel = viewModel, onBack = { navController.popBackStack() }) }
+        composable(Screen.NewPlant.route) { EditPlantScreen(plantId = null, viewModel = viewModel, onBack = { navController.popBackStack() }) }
 
-        composable(Screen.Search.route) {
-            SearchScreen(
-                viewModel = viewModel,
-                onPlantClick = { plant: PlantEntity ->
-                    viewModel.selectPlant(plant)
-                    navController.navigate(Screen.PlantDetail.createRoute(plant.id))
-                },
-                onBack = { navController.popBackStack() }
-            )
-        }
-
-        composable(Screen.DownloadImages.route) {
-            DownloadImagesScreen(viewModel = viewModel, onBack = { navController.popBackStack() })
-        }
-
-        composable(Screen.NewPlant.route) {
-            EditPlantScreen(plantId = null, viewModel = viewModel, onBack = { navController.popBackStack() })
-        }
-
-        composable(
-            route = Screen.EditPlant.route,
-            arguments = listOf(navArgument("plantId") { type = NavType.IntType })
-        ) { backStackEntry ->
+        composable(route = Screen.EditPlant.route, arguments = listOf(navArgument("plantId") { type = NavType.IntType })) { backStackEntry ->
             val plantId = backStackEntry.arguments?.getInt("plantId") ?: return@composable
             EditPlantScreen(plantId = plantId, viewModel = viewModel, onBack = { navController.popBackStack() })
         }
@@ -147,24 +83,13 @@ fun PlantNavGraph(viewModel: PlantViewModel) {
         composable(Screen.CameraIdentify.route) {
             CameraIdentifyScreen(
                 viewModel = viewModel,
-                onPlantClick = { plant: PlantEntity ->
-                    viewModel.selectPlant(plant)
-                    navController.navigate(Screen.PlantDetail.createRoute(plant.id))
-                },
-                onNavigateToPlantNetResult = { name, scientificName ->
-                    navController.navigate(Screen.PlantNetResult.createRoute(name, scientificName))
-                },
+                onPlantClick = { plant: PlantEntity -> viewModel.selectPlant(plant); navController.navigate(Screen.PlantDetail.createRoute(plant.id)) },
+                onNavigateToPlantNetResult = { name, scientificName -> navController.navigate(Screen.PlantNetResult.createRoute(name, scientificName)) },
                 onBack = { navController.popBackStack() }
             )
         }
 
-        composable(
-            route = Screen.PlantNetResult.route,
-            arguments = listOf(
-                navArgument("name") { type = NavType.StringType },
-                navArgument("scientificName") { type = NavType.StringType }
-            )
-        ) { backStackEntry ->
+        composable(route = Screen.PlantNetResult.route, arguments = listOf(navArgument("name") { type = NavType.StringType }, navArgument("scientificName") { type = NavType.StringType })) { backStackEntry ->
             val name = backStackEntry.arguments?.getString("name") ?: ""
             val scientificName = backStackEntry.arguments?.getString("scientificName") ?: ""
             PlantNetResultScreen(name = name, scientificName = scientificName, onBack = { navController.popBackStack() })
